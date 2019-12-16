01bd7014acdc9c0eb9dcf41932b7c707015a3816
Markus <Markusbolsen@gmail.com>
2019-12-16 10:26:20 +0100

    Fixed Doors + Added Jump
    * Fixed Doors (Smoth open and close + no overshooting of doors)
    + Jump added

--- InteractiveGame/Assets/Scripts/OpenDoors.cs
+++ InteractiveGame/Assets/Scripts/OpenDoors.cs
@@ -1,51 +1,65 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 
 public class OpenDoors : MonoBehaviour {
 
 	//fields
+	private Vector3 startPosL, startPosR;
 	private GameObject doorL;
 	private GameObject doorR;
 
 	public bool closed;
-	private bool last;
+	private bool last, change;
 
 	public float speed;
-	private float time;
 
 	// Start is called before the first frame update
 	void Start() {
 		//sætter variabler
 		last = closed = true;
 
-		time = 0;
-
 		//sætter game objects
 		doorL = transform.Find("DoorL").gameObject;
 		doorR = transform.Find("DoorR").gameObject;
+
+		startPosL = doorL.transform.position;
+		startPosR = doorR.transform.position;
 	}
 
 	// Update is called once per frame
 	void Update() {
 
-		//hvis closed endre sig
+		//checker for om der er en ændring
 		if(closed != last) {
-
-			//hvis den lukker sig
-			if(closed) {
-				doorL.transform.Translate(+speed * Time.deltaTime, 0, 0);
-				doorR.transform.Translate(-speed * Time.deltaTime, 0, 0);
-			} else { //hivs den åbner sig
-				doorL.transform.Translate(-speed * Time.deltaTime, 0, 0);
-				doorR.transform.Translate(+speed * Time.deltaTime, 0, 0);
-			}
-
-			//tilføger tid
-			time += Time.deltaTime;
-			if(time >= 1.5) {
-				time = 0;
-				last = closed;
-			}
+			last = closed;
+			change = true;
 		}
 
+		//gør noget ved det
+		if(change) {
+			if(closed) { //lukker sig
+				float lastMag = (doorL.transform.position - startPosL).magnitude;
+
+				doorL.transform.Translate(+speed * Time.deltaTime, 0, 0);
+				doorR.transform.Translate(-speed * Time.deltaTime, 0, 0);
+
+				if((doorL.transform.position - startPosL).magnitude >= lastMag) {
+					change = false;
+					doorL.transform.position = startPosL;
+					doorR.transform.position = startPosR;
+				}
+
+			} else { //åbner sig
+
+				doorL.transform.Translate(-speed * Time.deltaTime, 0, 0);
+				doorR.transform.Translate(+speed * Time.deltaTime, 0, 0);
+
+				if((doorL.transform.position - startPosL).magnitude > 1.5f) {
+					change = false;
+					doorL.transform.position = startPosL - transform.right.normalized * 1.5f;
+					doorR.transform.position = startPosR + transform.right.normalized * 1.5f;
+				}
+			}
+		}
 	}
 }
