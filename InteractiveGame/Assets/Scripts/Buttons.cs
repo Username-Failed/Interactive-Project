84ca45cea5825630f02dd06bc6418f1c6dff7a0a
Markus <Markusbolsen@gmail.com>
2019-12-18 11:56:33 +0100

    Added Menu
    + Menu (Title and Play Button)

---
+++ InteractiveGame/Assets/Scripts/Buttons.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour {
+    
+    //LoadGame er kaldet når der trykkes på play knappen
+    public void LoadGame() {
+        //Loader game scenen som har tallet '1'
+        SceneManager.LoadSceneAsync(1);
+    }
+}
